70450fc0e9f227c98d152e75af59638e9ab29304
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-09-30 11:07:17 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
+function ProjectList({ projects, info }) {
   console.log(projects[0].name);
   let pro = projects.map((item)=>{
     return( <div key={item.id}><h2 > {item.name}</h2>
@@ -16,7 +16,7 @@ function ProjectList({ projects }) {
       {pro}
       <div id="project-list"> 
       
-      <ProjectItem technologies=  {projects[0].technologies}/>
+      <ProjectItem technologies={projects[0].technologies}  />
       <ProjectItem technologies={projects[1].technologies}/>
       <ProjectItem technologies={projects[2].technologies}/>
       
